96425df49ac5581930206905fe29e56b59b23ea8
Mav <mespin11@jh.edu>
2024-08-17 10:09:10 -0700

    started basix header component

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import Hero from "@/components/hero";
+
+export default function Home() {
+  return (
+    <main >
+      <div className="max-w-7xl w-full">
+        <Header />
+        {/* <Hero /> */}
+      </div>
+    </main>
+  );
+}
